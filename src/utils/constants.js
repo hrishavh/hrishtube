7a149ca6473549d38b1e96b9e8701abcaeef501d
Hrishabh Tomar <hrishabhtomar14@gmail.com>
2023-04-30 15:57:43 +0530

    sr tech removed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -18,7 +18,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon  />, },
-  { name: 'SR Tech Academy ', icon: <CodeIcon />, },
+  // { name: 'SR Tech Academy ', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
